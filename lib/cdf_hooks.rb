993103af623d257b1cd65677b61f790f2e005496
Joshua Jacobs <josh@mandala-designs.com>
2011-07-01 15:17:26 -0400

    Adding navigation for po file management

--- lib/cdf_hooks.rb
+++ lib/cdf_hooks.rb
@@ -1,7 +1,7 @@
 class CdfHooks < Spree::ThemeSupport::HookListener
 
   insert_after :admin_tabs do
-     "<%= tab :fulfillment, :route => :admin_fulfillment %>"
+     "<%= tab :fulfillment, :po_files, :route => :admin_fulfillment %>"
   end
 
 end
