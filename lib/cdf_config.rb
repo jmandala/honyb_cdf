e6f3ef6bb7bc420884e469d7c95cbc3e969c56de
Joshua Jacobs <josh@mandala-designs.com>
2011-07-06 06:07:17 -0400

    created CdfConfig::data_lib_in paths

--- lib/cdf_config.rb
+++ lib/cdf_config.rb
@@ -10,11 +10,22 @@ class CdfConfig
     end
     
     "#{self.data_lib_path}/out/#{year}"
+  end
 
+  def self.data_lib_in_root(year='')
+    if year.empty?
+      return "#{self.data_lib_path}/in"
+    end
+
+    "#{self.data_lib_path}/in/#{year}"
   end
 
   def self.current_data_lib_out
     self.data_lib_out_root self.this_year
+  end
+
+  def self.current_data_lib_in
+    self.data_lib_in_root self.this_year
   end
 
   private
