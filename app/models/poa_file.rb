1172456b4fa8ca54da5fc776af3525cb3cb761b6
Joshua Jacobs <josh@mandala-designs.com>
2011-07-06 16:03:37 -0400

    refactored to delegate more work to each class

--- app/models/poa_file.rb
+++ app/models/poa_file.rb
@@ -25,12 +25,15 @@ class PoaFile < ActiveRecord::Base
   def populate
     #todo! parse & load
     p = parsed
-    populate_header(p)
-    populate_poa_order_header(p)
+
+    header = p[:header].first
+    header[:poa_type_id] = PoaType.find_by_code(header[:poa_type]).try(:id)
+    update_from_hash header, :excludes => [:file_name]
+
+    PoaOrderHeader.populate(p)
 
     save!
   end
-
 
   def path
     "#{CdfConfig::data_lib_in_root(created_at.strftime("%Y"))}/#{file_name}"
@@ -74,50 +77,8 @@ class PoaFile < ActiveRecord::Base
         h.spacer 4
       end
 
-      d.poa_order_header do |l|
-        l.trap { |line| line[0, 2] == '11' }
-        l.template :boundary
-        l.toc 13
-        l.po_number 22
-        l.icg_ship_to_account_number 7
-        l.icg_san 7
-        l.po_status 1
-        l.acknowledgement_date 6
-        l.po_date 6
-        l.po_cancellation_date 6
-        l.spacer 5
-      end
-    end
-  end
-
-
-  private
-
-  def populate_header(p)
-    header = p[:header].first
-    header[:poa_type_id] = PoaType.find_by_code(header[:poa_type]).try(:id)
-    update_from_hash header, :excludes => [:file_name]
-  end
-
-  def populate_poa_order_header(p)
-    p[:poa_order_header].each do |data|
-      order = Order.find_by_number(data[:po_number])
-
-      if order.nil?
-        raise ActiveRecord::RecordNotFound.new("No Order found with number: #{data[:po_number]}")
-      end
-
-      if order.po_file.nil?
-        raise ActiveRecord::RecordNotFound.new("No PO File found for order number: #{data[:po_number]}")
-      end
-
-      po_file = order.po_file
-
-      poa_order_header = PoaOrderHeader.find_or_create_by_po_file_id_and_poa_file_id(po_file.id, id)
-
-      data[:po_status_id] = PoStatus.find_by_code(data[:po_status]).try(:id)
-
-      poa_order_header.update_from_hash(data)
+      PoaOrderHeader.spec(d)
+      
     end
   end
 
--- app/models/poa_order_header.rb
+++ app/models/poa_order_header.rb
@@ -6,4 +6,41 @@ class PoaOrderHeader < ActiveRecord::Base
   belongs_to :po_file
   belongs_to :po_status
 
+  def self.spec(d)
+    d.poa_order_header do |l|
+      l.trap { |line| line[0, 2] == '11' }
+      l.template :boundary
+      l.toc 13
+      l.po_number 22
+      l.icg_ship_to_account_number 7
+      l.icg_san 7
+      l.po_status 1
+      l.acknowledgement_date 6
+      l.po_date 6
+      l.po_cancellation_date 6
+      l.spacer 5
+    end
+  end
+
+  def self.populate(p)
+    p[:poa_order_header].each do |data|
+      order = Order.find_by_number(data[:po_number])
+
+      if order.nil?
+        raise ActiveRecord::RecordNotFound.new("No Order found with number: #{data[:po_number]}")
+      end
+
+      if order.po_file.nil?
+        raise ActiveRecord::RecordNotFound.new("No PO File found for order number: #{data[:po_number]}")
+      end
+
+      po_file = order.po_file
+
+      poa_order_header = PoaOrderHeader.find_or_create_by_po_file_id_and_poa_file_id(po_file.id, id)
+
+      data[:po_status_id] = PoStatus.find_by_code(data[:po_status]).try(:id)
+
+      poa_order_header.update_from_hash(data)
+    end
+  end
 end
