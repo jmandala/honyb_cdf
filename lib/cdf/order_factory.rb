7b07ecfbe772116567fd9840970ff61bf3e92a6d
Joshua Jacobs <josh@mandala-designs.com>
2011-08-31 09:09:17 -0400

    - dashboard/index.html.haml: converted button to form - order_factory.rb: added scenarios, fixed #init_test -> #to_test

--- lib/cdf/order_factory.rb
+++ lib/cdf/order_factory.rb
@@ -2,8 +2,15 @@ require File.join File.dirname(__FILE__ ), '../../../spec/factories'
 
 class Cdf::OrderFactory
   
+  SCENARIOS = [
+  {:id => 1, :name => 'single order/single line/single quantity'},
+  {:id => 2, :name => 'single order/single line/multiple quantity'},
+  {:id => 3, :name => 'single order/multiple lines/single quantity'},
+  {:id => 4, :name => 'single order/multiple lines/multiple quantity'}
+  ]
+  
   def self.new
     order = FactoryGirl.create(:order)
-    order.init_test
+    order.to_test
   end
 end
