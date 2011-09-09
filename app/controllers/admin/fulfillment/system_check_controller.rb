e4036a0a5087bff4aaec9e12250687fd30248b90
Joshua Jacobs <josh@mandala-designs.com>
2011-09-09 14:05:50 -0400

    Green system_check controller & view

---
+++ app/controllers/admin/fulfillment/system_check_controller.rb
@@ -0,0 +1,13 @@
+class Admin::Fulfillment::SystemCheckController < Admin::BaseController
+  respond_to :html, :xml, :json
+  
+  def index
+  end
+
+  def order_check
+  end
+
+  def ftp_check
+  end
+
+end
