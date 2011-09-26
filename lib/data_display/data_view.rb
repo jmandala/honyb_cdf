c9a21b3c1e453c284dda8f1a9416673773d031e4
Joshua Jacobs <josh@mandala-designs.com>
2011-09-26 12:40:40 -0400

    initial data view handling

--- lib/data_display/data_view.rb
+++ lib/data_display/data_view.rb
@@ -1,5 +1,9 @@
 module DataDisplay
   class DataView
-  
+    def self.data_pair(field_name)
+      """
+      <div class='#{field_name}'><label>#{field_name}</label></div>
+      """
+    end
   end
 end
