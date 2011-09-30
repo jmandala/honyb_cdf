228630ee208a0082daa0a26607d0b8dee03df1c1
Joshua Jacobs <josh@mandala-designs.com>
2011-09-30 12:12:26 -0400

    - order_builder.rb: modified to allow for sending to po boxes

--- lib/cdf/order_builder.rb
+++ lib/cdf/order_builder.rb
@@ -5,14 +5,15 @@ class Cdf::OrderBuilder
       {:id => 2, :name => 'single order/single line/multiple quantity', :line_item_qty => 2},
       {:id => 3, :name => 'single order/multiple lines/single quantity', :line_item_count => 2},
       {:id => 4, :name => 'single order/multiple lines/multiple quantity', :line_item_count => 2, :line_item_qty => 2},
-      {:id => 5, :name => 'single order/multiple lines/multiple quantity: Hawaii', :line_item_count => 2, :line_item_qty => 2, :ship_location => :HI},
-      {:id => 6, :name => 'single order/multiple lines/multiple quantity: Alaska', :line_item_count => 2, :line_item_qty => 2, :ship_location => :AK},
-      {:id => 7, :name => 'single order/multiple lines/multiple quantity: Puerto Rico', :line_item_count => 2, :line_item_qty => 2, :ship_location => :PR},
-      {:id => 8, :name => 'single order/multiple lines/multiple quantity: USVI', :line_item_count => 2, :line_item_qty => 2, :ship_location => :VI},
-      {:id => 9, :name => 'single order/multiple lines/multiple quantity: UM', :line_item_count => 2, :line_item_qty => 2, :ship_location => :UM},
-      {:id => 10, :name => 'single order/multiple lines/multiple quantity: AE', :line_item_count => 2, :line_item_qty => 2, :ship_location => :AE},
-      {:id => 11, :name => 'single order/multiple lines/multiple quantity: AA', :line_item_count => 2, :line_item_qty => 2, :ship_location => :AA},
-      {:id => 12, :name => 'single order/multiple lines/multiple quantity: AP', :line_item_count => 2, :line_item_qty => 2, :ship_location => :AP},
+      {:id => 5, :name => 'single order/multiple lines/multiple quantity: Hawaii', :line_item_count => 2, :line_item_qty => 2, :state_abbr => :HI},
+      {:id => 6, :name => 'single order/multiple lines/multiple quantity: Alaska', :line_item_count => 2, :line_item_qty => 2, :state_abbr => :AK},
+      {:id => 7, :name => 'single order/multiple lines/multiple quantity: Puerto Rico', :line_item_count => 2, :line_item_qty => 2, :state_abbr => :PR},
+      {:id => 8, :name => 'single order/multiple lines/multiple quantity: USVI', :line_item_count => 2, :line_item_qty => 2, :state_abbr => :VI},
+      {:id => 9, :name => 'single order/multiple lines/multiple quantity: UM', :line_item_count => 2, :line_item_qty => 2, :state_abbr => :UM},
+      {:id => 10, :name => 'single order/multiple lines/multiple quantity: AE', :line_item_count => 2, :line_item_qty => 2, :state_abbr => :AE},
+      {:id => 11, :name => 'single order/multiple lines/multiple quantity: AA', :line_item_count => 2, :line_item_qty => 2, :state_abbr => :AA},
+      {:id => 12, :name => 'single order/multiple lines/multiple quantity: AP', :line_item_count => 2, :line_item_qty => 2, :state_abbr => :AP},
+      {:id => 13, :name => 'single order/single lines/single quantity: P.O. Box', :address1 => 'P.O. Box 123'},
   ]
 
   def self.create_for_scenarios(scenarios=[])
@@ -35,7 +36,7 @@ class Cdf::OrderBuilder
   end
 
   def self.completed_test_order(opts={})
-    opts[:ship_location] ||= :ME
+    opts[:state_abbr] ||= :ME
     opts[:line_item_count] ||= 1
     opts[:line_item_qty] ||= 1
     opts[:backordered_line_item_count] ||= 0
@@ -43,7 +44,7 @@ class Cdf::OrderBuilder
 
     order = Order.new_test
 
-    address = create_address opts[:ship_location]
+    address = create_address opts
 
     order.bill_address = address
     order.ship_address = address
@@ -88,9 +89,12 @@ class Cdf::OrderBuilder
     all_methods.first
   end
 
-  def self.create_address(state_abbr)    
+  def self.create_address(opts)    
+    opts[:state_abbr] ||= :ME
+    opts[:address1] ||= "10 Lovely Street" 
     my_addr = address
-    my_addr.state = State.find_by_abbr!(state_abbr)
+    my_addr.address1 = opts[:address1]
+    my_addr.state = State.find_by_abbr!(opts[:state_abbr])
     my_addr.country = my_addr.state.country
     my_addr.save!
     my_addr
