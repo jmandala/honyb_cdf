f7c4fdea948e444c3c07a9929461be5e511bce56
Joshua Jacobs <josh@mandala-designs.com>
2011-10-24 11:03:17 -0400

    - asn_shipment_detail.rb: re-enabled shipment.ship! - shipment_decorator.rb: allow shipments to have parent-child relationships; #create_child will create a child with the inventory units inheriting base values from parent - cdf_compliance_spec.rb: ensure each split shipment has correct inventory unit cost - shipment_decorator_spec.rb: add specs for parent-child relationship

--- app/models/shipment_decorator.rb
+++ app/models/shipment_decorator.rb
@@ -1,5 +1,7 @@
 Shipment.class_eval do
 
+  has_many :children, :class_name => Shipment.name, :foreign_key => 'parent_id'
+  belongs_to :parent, :class_name => Shipment.name, :foreign_key => 'parent_id'
 
   # overwriting core method in order to prevent email from being sent each time
   # the shipment is marked shipped
@@ -8,5 +10,16 @@ Shipment.class_eval do
     inventory_units.each {|u| u.ship! unless u.state?('shipped') }
   end
  
+  def create_child(inventory_units)
+    child = Shipment.create!(
+        :order => order, 
+        :shipping_method => shipping_method,
+        :address => address,
+        :inventory_units => inventory_units,
+        :parent => self
+    )
+    self.children << child
+    child
+  end
 
 end
